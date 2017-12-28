a6d2ad8586cabf133da594e01a8a2a1d9c984670
Paulo Ernesto <pemn@users.noreply.github.com>
2017-12-28 09:26:08 -0200

    Delete gui_example3_standalone.py

--- gui_example3_standalone.py
+++
@@ -1,9 +0,0 @@
-#!python
-from _gui import usage_gui
-
-def main(*args):
-  from tkinter.messagebox import showinfo
-  showinfo(__file__, "Custom Business Logic for script\n" + __file__ + "\narguments:\n" + str(args))
-
-if __name__=="__main__":
-  usage_gui("usage: script.py input*csv field:input choice=good,bad,ugly output*pdf,png")
