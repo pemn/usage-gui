919f464e3e2124a0afa635d2958a40c1d47ee9b3
Paulo Ernesto <pemn@users.noreply.github.com>
2018-03-27 14:21:47 -0300

    Add files via upload

--- examples/gui_example4_vulcan.py
+++ examples/gui_example4_vulcan.py
@@ -1,9 +1,4 @@
 #!python
 from _gui import usage_gui
 
-def main(*args):
-  from tkinter.messagebox import showinfo
-  showinfo(__file__, "Custom Business Logic for script\n" + __file__ + "\narguments:\n" + str(args))
-
-if __name__=="__main__":
-  usage_gui("usage: script.py block_model*bmf bm_variable:block_model database*isis db_variable:database dgd*dgd.isis layer:dgd surfaces#tri*00t output*csv")
+usage_gui(None)
