14e85b90225006e718f7f5e4e6ff72dfa69510cb
Paulo Ernesto <pemn@users.noreply.github.com>
2019-06-13 09:07:55 -0300

    Add files via upload

--- examples/gui_example1_showcase.py
+++ examples/gui_example1_showcase.py
@@ -7,13 +7,15 @@
 # v1.1 08/2015 developer A
 # v1.0 01/2015 original creator
 '''
-usage: $0 input_csv*csv variable_csv:input_csv some_trully_realy_long_label plain_logical@ table1#variable:input_csv enabler_logical@5 conditional_logical@ conditional_choice1=red,green,blue conditional_entry1 table2#dbfile*csv#key:dbfile output_image*pdf,png,gif,jpg out_of_reach
+usage: $0 input_csv*csv variable_csv:input_csv some_trully_realy_long_label plain_logical@ table1#variable:input_csv enabler_logical@3 conditional_entry conditional_logical@ conditional_choice1=red,green,blue out_of_reach radio_button1%red,green,blue table2#dbfile*csv#key:dbfile output_image*pdf,png,gif,jpg
 '''
 from _gui import usage_gui
 
 def main(*args):
   from tkinter.messagebox import showinfo
+  # raise Exception("foo occurred")
   showinfo(__file__, "Custom Business Logic for script\n" + __file__ + "\narguments:\n" + str(args))
 
 if __name__=="__main__":
   usage_gui(__doc__)
+  
